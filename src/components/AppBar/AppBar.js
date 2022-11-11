7b96b25ec9cc97bd35e36ffb08a354d4d20fdc1d
Evgenii Horokhov <gorohovevgeniyvalerievich@gmail.com>
2022-11-11 21:40:41 +0200

    add

---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux';
+import { authSelectors } from 'redux/auth';
+import AuthForm from 'components/AuthForm';
+import Navigation from 'components/Navigation';
+import UserMenu from 'components/UserMenu';
+
+import styles from './Appbar.module.css';
+
+export default function AppBar() {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.nav}>
+        <Navigation />
+        {isLoggedIn ? <UserMenu /> : <AuthForm />}
+      </div>
+    </header>
+  );
+}
---
+++ src/redux/store.js
@@ -0,0 +1,42 @@
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { contactsApi } from 'services/contactsApi';
+import { authReducer } from './auth';
+import contactsReducer from './contacts/contactsReducer';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    contactsReducer,
+    [contactsApi.reducerPath]: contactsApi.reducer,
+  },
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(contactsApi.middleware);
+  },
+  logger,
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+const persistor = persistStore(store);
+export { store, persistor };
