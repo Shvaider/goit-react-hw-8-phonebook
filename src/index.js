1a1cd0fe2c22344aef6738eeeaa00e638960cd0d
Evgenii Horokhov <gorohovevgeniyvalerievich@gmail.com>
2022-11-12 17:11:47 +0200

    HW-8

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 import { persistor, store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/goit-react-hw-8-phonebook">
@@ -16,5 +16,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
